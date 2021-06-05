810ac8233483fb49c5e572ab8d4871722f26bb8c
Win81VMUser <78744628+Win81VMUser@users.noreply.github.com>
2021-06-05 16:02:42 +0200

    Update start-debian.sh

--- Stuff/start-debian.sh
+++ Stuff/start-debian.sh
@@ -1,4 +1,4 @@
 clear
 echo Entering Debian 10...
 proot-distro login debian-buster --isolated
-echo Leaved Debian 10. To launch again type ./debian.sh
+echo Leaved Debian 10. To launch again type ./start-debian.sh
