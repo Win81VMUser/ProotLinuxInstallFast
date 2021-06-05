3a9efceaaed68b2da973137b66ad582985ee9a7d
Win81VMUser <78744628+Win81VMUser@users.noreply.github.com>
2021-06-05 16:04:43 +0200

    Update start-ubuntu18.sh

--- Stuff/start-ubuntu18.sh
+++ Stuff/start-ubuntu18.sh
@@ -1,4 +1,4 @@
 clear
 echo Entering Ubuntu-18.04...
 proot-distro login ubuntu-18.04 --isolated
-echo Leaved Debian 10. To launch again type ./start-ubuntu18.sh
+echo Leaved Ubuntu-18.04. To launch again type ./start-ubuntu18.sh
