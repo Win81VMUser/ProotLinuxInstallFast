ced056808d398e986dc30dc09c45d6c346aee568
Win81VMUser <78744628+Win81VMUser@users.noreply.github.com>
2021-06-05 16:18:34 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+echo Downloading scripts...
+wget https://raw.githubusercontent.com/Win81VMUser/ProotLinuxInstallFast/main/Stuff/install-ubuntu20.sh
+wget https://raw.githubusercontent.com/Win81VMUser/ProotLinuxInstallFast/main/Stuff/install-ubuntu18.sh
+wget https://raw.githubusercontent.com/Win81VMUser/ProotLinuxInstallFast/main/Stuff/install-debian.sh
+wget https://raw.githubusercontent.com/Win81VMUser/ProotLinuxInstallFast/main/Stuff/remove-debian.sh
+wget https://raw.githubusercontent.com/Win81VMUser/ProotLinuxInstallFast/main/Stuff/remove-ubuntu18.sh
+wget https://raw.githubusercontent.com/Win81VMUser/ProotLinuxInstallFast/main/Stuff/remove-ubuntu20.sh
+wget https://raw.githubusercontent.com/Win81VMUser/ProotLinuxInstallFast/main/Stuff/reset-ubuntu20.sh
+wget https://raw.githubusercontent.com/Win81VMUser/ProotLinuxInstallFast/main/Stuff/reset-ubuntu18.sh
+wget https://raw.githubusercontent.com/Win81VMUser/ProotLinuxInstallFast/main/Stuff/reset-debian.sh
