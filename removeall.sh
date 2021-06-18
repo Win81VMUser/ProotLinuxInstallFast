a12eef9d0eb14838010a45a98820d6819866cd7a
Win81VMUser <78744628+Win81VMUser@users.noreply.github.com>
2021-06-18 17:44:43 +0200

    Update removeall.sh

--- removeall.sh
+++ removeall.sh
@@ -1,4 +1,10 @@
 echo This will remove ALL OF DISTROS AND FILES!!!
+echo Please make sure YOU WANT DO IT!!! DELETING IN 5 SECONDS!!! & sleep 2
+echo DELETING IN 4 & sleep 1
+echo DELETING IN 3 & sleep 1
+echo DELETING IN 2 & sleep 1
+echo DELETING IN 1 & sleep 1
+echo DELETING STARTED!!! LAST 2 SECONDS TO CLOSE THIS OR DELETE FILES!!! & sleep 3
 echo Removing Distros...
 bash debian/remove-debian.sh
 bash ubuntu18/remove-ubuntu18.sh
@@ -14,4 +20,4 @@ clear
 echo Removing programs files and scripts...
 rm -rf *
 rm *
-echo Done!
+echo Done! To setup again please run this: wget https://raw.githubusercontent.com/Win81VMUser/ProotLinuxInstallFast/main/setup.sh
