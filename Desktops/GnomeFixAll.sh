0ca03cddd5d75b598bbf711522cdfa3fdb60c19e
Win81VMUser <78744628+Win81VMUser@users.noreply.github.com>
2021-08-01 09:31:12 +0200

    Update GnomeFixAll.sh

--- Desktops/GnomeFixAll.sh
+++ Desktops/GnomeFixAll.sh
@@ -1,15 +1,17 @@
 echo "Welcome to GnomeFixAll.sh! Installation will begin soon..."
-echo :Installation will start in 5 seconds, press ctrl+c to cancel or wait to start." & sleep 2
+echo "Installation will start in 5 seconds, press ctrl+c to cancel or wait to start." & sleep 2
 echo Starting in 4... & sleep 1
 echo Starting in 3... & sleep 1
 echo Starting in 2... & sleep 1
 echo Starting in 1... & sleep 1
 echo "Dont touch anything during installation..."
-sudo apt install light-themes gnome-session-flashback gnome-settings-daemon gnome-terminal metacity nautilus gnome-tweak-tool tigervnc-standalone-server dbus* dbus-* -y
+sudo apt install light-themes gnome-session-flashback gnome-settings-daemon gnome-terminal metacity nautilus gnome-tweak-tool dbus* dbus-* -y
 clear
 echo "Fixing files and gnome..."
 sudo service dbus start
 sudo service dbus status & sleep 1
+echo Installing VNC
+sudo apt install tigervnc-standalone-server
 echo "Seems to be fine... Lets start desktop!"
 echo "To kill vnc type vncserver -kill :*"
 sudo service dbus start && tigervncserver -xstartup /usr/lib/gnome-flashback/gnome-flashback-metacity
