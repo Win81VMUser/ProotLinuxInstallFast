33355be1b77bd37e413a6d552803812bcc473757
Win81VMUser <78744628+Win81VMUser@users.noreply.github.com>
2021-08-01 18:07:40 +0200

    Create KdeCoreInstaller.sh

---
+++ Desktops/KdeCoreInstaller.sh
@@ -0,0 +1,18 @@
+echo The installation is core and some programs or files might not work or be not included in this Desktop Environment!
+echo Kde-Plasma-Desktop, a core version of KDE is small but takes around 2 gb so better have 4 gb free on storage.
+echo ... & sleep 4
+clear
+echo Installation will begin in 5 seconds!
+echo Starting in 5
+echo Starting in 4
+echo Starting in 3
+echo Starting in 2
+echo Starting in 1
+echo Installation will begin soon!
+clear
+sudo apt install kde-plasma-desktop -y
+clear
+echo Installing VNC Server...
+apt install tigervnc-standalone-server -y
+clear
+echo Done!
